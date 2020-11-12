c5f5514cf84c4ccc6cdbe56d985a333d1ea50894
Your Name <you@example.com>
2020-11-12 16:30:27 +0200

    commit 1

---
+++ client/client.py
@@ -0,0 +1,18 @@
+
+import urllib
+
+from flask import Flask,jsonify 
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/serach/distributed systems', methods=['GET'] )
+def hello_world():
+	return urllib.request.urlopen('http://0.0.0.0:5001/').read()
+   
+
+	
+if __name__ == '__main__':
+    app.run(debug = True)    
