3f4db91b2d5f34c312eb42a2fc8f654c9af25a52
Your Name <you@example.com>
2020-11-12 18:41:04 +0200

    commit 1

--- client/client.py
+++ client/client.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 
 
-@app.route('/', methods=['GET'] )
-def hello_world():
+@app.route('/search/distributed systems', methods=['GET'] )
+def search_dos():
 	return urllib.request.urlopen('http://0.0.0.0:5001/query_by_subject/distributed systems').read()
    
 
