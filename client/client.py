fbc6833911ab2ab18ac1e04f4474aa9a4868afd8
Your Name <you@example.com>
2020-11-12 16:38:17 +0200

    commit 2

--- client/client.py
+++ client/client.py
@@ -12,7 +12,9 @@ app = Flask(__name__)
 def hello_world():
 	return urllib.request.urlopen('http://0.0.0.0:5001/').read()
    
-
+@app.route('/serach/graduate school', methods=['GET'] )
+def hello_world():
+	return urllib.request.urlopen('http://0.0.0.0:5001/').read()
 	
 if __name__ == '__main__':
     app.run(debug = True)    
