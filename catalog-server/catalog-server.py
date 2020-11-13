0109bdc2fba467e98bd398c07319011ced27d0fd
Amr Dweikat <amrdweikat@hotmail.com>
2020-11-13 09:17:07 +0200

    commit 3

--- catalog-server/catalog-server.py
+++ catalog-server/catalog-server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 
 app = Flask(__name__)
@@ -7,13 +7,16 @@ app = Flask(__name__)
 
 @app.route('/query_by_subject/distributed systems',methods=['GET'])
 def query_by_subject():
-	return "hello_world"
+	return jsonify({'id': 1,'title':'How to get a good grade in DOS in 20 minutes a day'},{"id" : 2,'title':'RPCs for Dummies'})
    
- 
+
+@app.route('/query_by_subject/graduate school',methods=['GET'])
+def query_by_subject_2():
+	return jsonify({'id': 3,'title':'Xen and the Art of Surviving Graduate School'},{"id" : 4,'title':'Cooking for the Impatient Graduate Student'}) 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug = True,port=5001)   
+    app.run(host='0.0.0.0',debug = True,port=5002)   
 
 
 
