c02260acb9db5563eac8b455cb5095707413efe6
Amr Dweikat <amrdweikat@hotmail.com>
2020-11-13 09:37:02 +0200

    commit 4

--- catalog-server/catalog-server.py
+++ catalog-server/catalog-server.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 
 
-@app.route('/query_by_subject/distributed systems',methods=['GET'])
+@app.route('/query_by_subject/distributed',methods=['GET'])
 def query_by_subject():
 	return jsonify({'id': 1,'title':'How to get a good grade in DOS in 20 minutes a day'},{"id" : 2,'title':'RPCs for Dummies'})
    
 
-@app.route('/query_by_subject/graduate school',methods=['GET'])
+@app.route('/query_by_subject/graduate',methods=['GET'])
 def query_by_subject_2():
 	return jsonify({'id': 3,'title':'Xen and the Art of Surviving Graduate School'},{"id" : 4,'title':'Cooking for the Impatient Graduate Student'}) 
 
