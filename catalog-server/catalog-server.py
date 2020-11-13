c0da682ccf97ab9495286677aebcc2f223234921
Amr Dweikat <amrdweikat@hotmail.com>
2020-11-13 14:13:45 +0200

    commit 5

--- catalog-server/catalog-server.py
+++ catalog-server/catalog-server.py
@@ -1,18 +1,42 @@
-from flask import Flask,jsonify
 
+
+from flask import Flask,jsonify
+import sqlite3
 
 app = Flask(__name__)
+
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect("books.sqlite")
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
 
 
 @app.route('/query_by_subject/distributed',methods=['GET'])
 def query_by_subject():
-	return jsonify({'id': 1,'title':'How to get a good grade in DOS in 20 minutes a day'},{"id" : 2,'title':'RPCs for Dummies'})
-   
+	conn   = db_connection()
+	cursor = conn.cursor()
+	cursor = conn.execute("SELECT id,title FROM book WHERE topic='distributed systems'")
+	books = [
+		dict(id=row[0],title=row[1])
+		for row in cursor.fetchall()
+	]
+	if books is not None:
+		return jsonify(books)
+
+	
 
 @app.route('/query_by_subject/graduate',methods=['GET'])
 def query_by_subject_2():
 	return jsonify({'id': 3,'title':'Xen and the Art of Surviving Graduate School'},{"id" : 4,'title':'Cooking for the Impatient Graduate Student'}) 
+
+
+
+
 
 
 if __name__ == '__main__':
