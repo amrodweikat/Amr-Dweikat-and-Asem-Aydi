a2bc064a244a493f889399e80435ec7ea043b787
Your Name <you@example.com>
2020-11-13 09:32:35 +0200

    commit 4

--- client/client.py
+++ client/client.py
@@ -10,8 +10,10 @@ app = Flask(__name__)
 
 @app.route('/search/distributed systems', methods=['GET'] )
 def search_dos():
-	return urllib.request.urlopen('http://0.0.0.0:5001/query_by_subject/distributed systems').read()
+	return  urllib.request.urlopen("http://127.0.0.1:5001/query_by_subject/distributed").read()
+	 
    
+
 
 @app.route('/search/graduate school', methods=['GET'] )
 def search_dos_2():
@@ -20,4 +22,4 @@ def search_dos_2():
 
 	
 if __name__ == '__main__':
-    app.run(debug = True)    
+    app.run(debug = True,host = "127.0.0.2", port ="5001")    
