b7401dd34a91747ca11319c8b7e8c6d150eb6e97
Your Name <you@example.com>
2020-11-13 09:47:18 +0200

    commit 6

--- client/client.py
+++ client/client.py
@@ -20,6 +20,10 @@ def search_dos_2():
 	return urllib.request.urlopen('http://127.0.0.1:5001/query_by_subject/graduate').read()
 
 
+@app.route('/lookup/1', methods=['GET'] )
+def lookup_1():
+	return urllib.request.urlopen('http://127.0.0.1:5001/query_by_item/1').read()
+
 	
 if __name__ == '__main__':
     app.run(debug = True,host = "127.0.0.2", port ="5001")    
